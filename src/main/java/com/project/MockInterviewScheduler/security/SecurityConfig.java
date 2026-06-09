57a41759d973f61634b916a98f4e28e0324f144a
abhi <abhisiktachakraborty3@gmail.com>
2026-06-09 14:35:15 +0530

    updating website http

--- src/main/java/com/project/MockInterviewScheduler/security/SecurityConfig.java
+++ src/main/java/com/project/MockInterviewScheduler/security/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://abhisikta-dev-portfolio.netlify.app"));
+        config.setAllowedOrigins(List.of("https://abhisikta-dev-portfolio.netlify.app",
+                "https://mockprepinterviewscheduler.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
